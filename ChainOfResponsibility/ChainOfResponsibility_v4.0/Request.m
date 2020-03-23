7bbb93d60659d8a63433efcb6f7de5eb51840cb9
housenkui <housenkui@gmail.com>
2020-03-23 09:40:19 +0800

    v4.0 完全模拟Serverlet中的FilterChain

---
+++ ChainOfResponsibility/ChainOfResponsibility_v4.0/Request.m
@@ -0,0 +1,13 @@
+//
+//  Request.m
+//  HTTPChainOfResponsibility
+//
+//  Created by 侯森魁 on 2020/3/22.
+//  Copyright © 2020 侯森魁. All rights reserved.
+//
+
+#import "Request.h"
+
+@implementation Request
+
+@end
---
+++ ChainOfResponsibility/ChainOfResponsibility_v4.0/Response.h
@@ -0,0 +1,18 @@
+//
+//  Response.h
+//  HTTPChainOfResponsibility
+//
+//  Created by 侯森魁 on 2020/3/22.
+//  Copyright © 2020 侯森魁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Response : NSObject
+@property (nonatomic,strong)NSMutableString *str;
+
+@end
+
+NS_ASSUME_NONNULL_END
